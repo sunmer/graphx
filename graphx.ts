2ad343197155f29fdf4794e37fe8f2b6aa3a1a90
nima <nima.boustanian@gmail.com>
2018-09-30 14:10:09 +0200

    added speeds for enemy and player

--- graphx.ts
+++ graphx.ts
@@ -4,7 +4,13 @@ const canvasHeight = 200;
 interface Entity {
   x: number;
   y: number;
-  speed: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+  speed: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9,
+  movement?: {
+    up: boolean,
+    down: boolean,
+    left: boolean,
+    right: boolean
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,8 +20,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const context = canvas.getContext("2d");
   const frame = { rate: 10, index: 0, length: 9 }
   const movementStep = 1;
-  let player: Entity = { x: 20, y: 20, speed: 1 };
-  let enemy: Entity = { x: 80, y: 80, speed: 9 };
+  let player: Entity = { 
+    x: 20, 
+    y: 20, 
+    speed: 6, 
+    movement: { 
+      up: false, 
+      down: false, 
+      left: false, 
+      right: false 
+    } 
+  };
+  let enemy: Entity = { 
+    x: 80, 
+    y: 80, 
+    speed: 9
+  };
   let radius = 5;
 
   let draw = (x, y, color) => {
@@ -29,6 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   document.onkeydown = event => {
+    player.movement.up = false;
+    player.movement.down = false;
+    player.movement.left = false;
+    player.movement.right = false;
+
     switch(event.keyCode) {
       case 65:
         console.log("a");
@@ -36,22 +61,26 @@ document.addEventListener("DOMContentLoaded", () => {
       case 40:
         if(!canMoveDown(player.y))
           return;
-        player.y += movementStep;
+        player.movement.up = false;
+        player.movement.down = true;
         break;
       case 39:
         if(!canMoveRight(player.x))
           return;
-        player.x += movementStep;
+        player.movement.left = false;
+        player.movement.right = true;
         break;
       case 38:
         if(!canMoveUp(player.y))
           return;
-        player.y -= movementStep;
+        player.movement.down = false;
+        player.movement.up = true;
         break;
       case 37:
         if(!canMoveLeft(player.x)) 
           return;
-        player.x -= movementStep;
+        player.movement.right = false;
+        player.movement.left = true;
         break;
     }
   }
@@ -77,12 +106,25 @@ document.addEventListener("DOMContentLoaded", () => {
     return enemy;
   }
 
+  let movePlayer = () => {
+    if(player.movement.up)
+      player.y -= movementStep;
+    if(player.movement.right)
+      player.x += movementStep;
+    if(player.movement.down)
+      player.y += movementStep;
+    if(player.movement.left)
+      player.x -= movementStep;
+  }
+
   setInterval(() => {
     if(frame.index === frame.length)
       frame.index = 0;
     
     if(frame.index % enemy.speed === 0)
       moveEnemy();
+    if(frame.index % player.speed === 0)
+      movePlayer();
       
     context.clearRect(0, 0, canvas.width, canvas.height);
     draw(player.x, player.y, "#dddddd");
