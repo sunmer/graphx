c95685386991110384d9106eaa816f5bdf372327
nima <nima.boustanian@gmail.com>
2018-10-04 22:44:21 +0200

    added counter in separate canvas

--- graphx.ts
+++ graphx.ts
@@ -5,23 +5,37 @@ const canvasWidth = 200;
 const canvasHeight = 200;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = <HTMLCanvasElement> document.getElementById("canvas");
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-  const context = canvas.getContext("2d");
+  const canvasGamePlay = <HTMLCanvasElement> document.getElementById("canvasGamePlay");
+  canvasGamePlay.width = canvasWidth;
+  canvasGamePlay.height = canvasHeight;
+
+  const canvasTimer = <HTMLCanvasElement> document.getElementById("canvasTimer");
+  canvasTimer.width = canvasWidth;
+  canvasTimer.height = canvasHeight;
+
+  const contextGamePlay = canvasGamePlay.getContext("2d");
+  const contextTimer = canvasTimer.getContext("2d");
   const frame = { rate: 10, index: 0, length: 9 }
   const movementStep = 1;
+  let timeElapsed = 0;
   let player: Entity = new Entity(20, 20, 1, "#dddddd");
-  let enemy: Entity = new Entity(50, 50, 4, "#c52323");
+  let enemy: Entity = new Entity(50, 50, 9, "#c52323");
   let playerController: Controller = new Controller(player);
   let enemyController: Controller = new Controller(enemy);
 
-  let draw = (entity: Entity) => {
-    context.beginPath();
-    context.rect(entity.x, entity.y, entity.radius, entity.radius);
-    context.fillStyle = entity.color;
-    context.closePath();
-    context.fill();
+  let drawEntity = (entity: Entity) => {
+    contextGamePlay.beginPath();
+    contextGamePlay.rect(entity.x, entity.y, entity.radius, entity.radius);
+    contextGamePlay.fillStyle = entity.color;
+    contextGamePlay.closePath();
+    contextGamePlay.fill();
+  }
+
+  let drawText = (text: string) => {
+    contextTimer.font = "30px Arial";
+    contextTimer.fillStyle = "#444";
+    contextTimer.textAlign = "center";
+    contextTimer.fillText(text, canvasTimer.width / 2, 50);
   }
 
   document.onkeydown = event => playerController.keydown(event.keyCode);
@@ -49,11 +63,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   let canMoveLeft = (entity: Entity) => entity.x > 0;
-  let canMoveRight = (entity: Entity) => entity.x + entity.radius < canvas.width;
-  let canMoveDown = (entity: Entity) => entity.y + entity.radius < canvas.height;
+  let canMoveRight = (entity: Entity) => entity.x + entity.radius < canvasWidth;
+  let canMoveDown = (entity: Entity) => entity.y + entity.radius < canvasHeight;
   let canMoveUp = (entity: Entity) => entity.y > 0;
 
-  setInterval(() => {
+  let gamePlay = setInterval(() => {
     if(frame.index === frame.length)
       frame.index = 0;
     
@@ -62,11 +76,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if(frame.index % player.speed === 0)
       movePlayer(playerController);
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    draw(player);
-    draw(enemy);
+    contextGamePlay.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawEntity(player);
+    drawEntity(enemy);
 
     frame.index++
   }, frame.rate);
+  
+  let timer = setInterval(() => {
+    contextTimer.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawText(timeElapsed.toString());
+    timeElapsed++;
+  }, 1000)
 
 }, false);
